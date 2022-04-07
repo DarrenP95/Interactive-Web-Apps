5cf49205152820f9feeae0ae81a9856a393ddd89
Darren Marco Paulse <d.paulse@lab.clicklearning.org>
2022-04-07 15:45:09 +0200

    update to main

---
+++ JS Debuggibg & Arithmetic/scripts/js-arithmetics.js
@@ -0,0 +1,41 @@
+// TITLE
+console.log(" ----- Area Of Shapes JS Arithmetic Challenge ----- \n\n")
+
+// Constant Variables:
+const pi = 3.1419;
+const half = 0.5;
+
+// Variables for measurements
+
+var Rectangle = length = 10, breadth = 17;
+var Circle = radius = 25;
+var Triangle = base = 20, height = 9;
+
+
+// Rectangle area calculation
+   var Rectangle = 10 * 17;
+
+// Append your rectangle answer:
+console.log("The area of the given rectangle is : " );
+
+
+console.log(Rectangle);
+
+
+// circle area calculation
+   var Circle =  25*25
+
+// Append your circle answer:
+console.log("The area of the given circle is : ");
+
+
+console.log(Circle);
+
+
+// triangle area calculation
+   var Triangle = (20*0.5) * 9
+
+// Append your triangle answer:
+console.log("The area of the given triangle is : " );
+
+console.log(Triangle);
